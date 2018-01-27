bd44d3ed05f2c3e5c243f19eea20a08c05b6e6ce
Jukka Edvardsson <jukka.edvardsson@wunderdog.fi>
2018-01-27 22:08:56 +0200

    12. workign with events

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,11 +2,28 @@ import React from 'react';
 import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+
+   /*  constructor(){
+        super();
+        this.gotoStore = this.gotoStore.bind(this);
+    } */
+
+
+
+    gotoStore(event) {
+        event.preventDefault();
+
+        console.log(this.storeInput.value);
+
+    
+    }
+
     render() {
         return (
-            <form className="store-selector"> 
-                <h2>Please input a store</h2>
-                <input type="text" required placeholder="Store name" defaultValue={getFunName()} />
+            <form className="store-selector" onSubmit={(e)=> this.gotoStore(e)}> 
+                <h2>Please input a store</h2> 
+                <input type="text" required placeholder="Store name" 
+                    defaultValue={getFunName()} ref={(input)=>{this.storeInput = input}}/>
                 <button type="submit">Visit Store</button>
             </form>
         )
