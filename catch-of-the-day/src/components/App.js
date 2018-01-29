5735f66f3818bfda094755fbf38ad5c8af7acc15
Jukka Edvardsson <jukka.edvardsson@wunderdog.fi>
2018-01-29 14:12:49 +0200

    15. jsx

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -3,6 +3,7 @@ import Header from './Header'
 import Order from './Order';
 import Inventory from './Inventory';
 import sampleFishes from '../sample-fishes';
+import Fish from './Fish';
 
 class App extends React.Component {
     constructor() {
@@ -39,8 +40,16 @@ class App extends React.Component {
         <div className="catch-of-the-day">
             <div className="menu">
                 <Header tagline="Automatisoi kaiken"/>
+                <ul>
+                   {
+                       Object.
+                       keys(this.state.fishes)
+                   .map(key => <Fish key={key} details={this.state.fishes[key] } />)
+                
+                   }
+                </ul>
             </div>
-            <Order />
+            <Order /> 
             <Inventory addFish={ this.addFish } loadSamples={ this.loadSamples} />
         </div>
         )
