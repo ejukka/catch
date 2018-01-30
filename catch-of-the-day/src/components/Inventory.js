6f6b5fb41fc645ceefd3871f0b958de1a66e3fbc
Jukka Edvardsson <jukka.edvardsson@wunderdog.fi>
2018-01-30 13:14:46 +0200

    20 - Bi-directional Data Flow and Live State Editing

--- catch-of-the-day/src/components/Inventory.js
+++ catch-of-the-day/src/components/Inventory.js
@@ -2,10 +2,50 @@ import React from 'react';
 import AddFishForm from './AddFishForm'
 
 class Inventory extends React.Component {
+
+    constructor() {
+        super();
+        this.renderInventory = this.renderInventory.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(e, key) {
+        const fish = this.props.fishes[key];
+
+        //take copy of the fish
+        const updatedFish = {
+            ...fish,
+            [e.target.name]: e.target.value
+        };
+        this.props.updateFish(key, updatedFish);
+    }
+
+    renderInventory(key) {
+        const fish = this.props.fishes[key];
+        return(
+            <div className="" key={key}>
+                <input type="text" name="name" value={fish.name} placeholder="test" onChange={(e) => this.handleChange(e, key)} />
+                <input type="text" name="price" value={fish.price} placeholder="test" onChange={(e) => this.handleChange(e, key)} />
+                
+                    <select type="text" name="status" value={fish.status} placeholder="test" onChange={(e) => this.handleChange(e, key)} >
+                        <option value="available">Fresh!</option>
+                        <option value="unavailable">Sold Out!</option>
+                    </select>
+
+                <textarea type="text" name="desc" value={fish.desc} placeholder="test" onChange={(e) => this.handleChange(e, key)} >
+                </textarea>
+                <input type="text" name="image" value={fish.image} placeholder="test" onChange={(e) => this.handleChange(e, key)} />
+            </div>
+        )
+    }
+
     render() {
         return (
             <div>
                 <h2>Inventory</h2>
+                {
+                    Object.keys(this.props.fishes).map(this.renderInventory)
+                }
                 <AddFishForm addFish={this.props.addFish} />
                 <button onClick={this.props.loadSamples}>Load sample fishes
                 </button>
